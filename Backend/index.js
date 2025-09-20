8884c2760af5fbe0f71c7d0caefabc0d6ccf1196
sahil gusain <gusainsahil714@gmail.com>
2025-09-20 12:25:32 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('./middleware/logger');
+const shortUrlsRouter = require('./routes/shorturls');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(logger);
+
+
+app.use('/shorturls', shortUrlsRouter);
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
